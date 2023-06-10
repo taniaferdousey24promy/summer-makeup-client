430513ae564baa5d6a5d44bfe55074b56131f0f2
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-06-10 18:27:55 +0600

    Commiting class.json

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Banner from "../Banner/Banner";
+import PopularClasses from "../PopularClasses/PopularClasses";
 
 
 const Home = () => {
     return (
-        <div>
-            <h2>this is home</h2>
+        <div className="mt-12">
             <Banner></Banner>
+            <PopularClasses></PopularClasses>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
   );
   return (
     <div>
-      <div  className="navbar bg-base-300 rounded bg-transparent">
+      <div  className="navbar bg-base-300 rounded bg-transparent mt-12 ">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
