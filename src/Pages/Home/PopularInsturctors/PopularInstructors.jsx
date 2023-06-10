392f48dfb92a2d9330e1b96737dade10fd9aceae
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-06-11 01:13:40 +0600

    Commiting popular instructors.

--- src/Pages/Home/PopularInsturctors/PopularInstructors.jsx
+++ src/Pages/Home/PopularInsturctors/PopularInstructors.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useRef} from "react";
+import { useRef } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -8,13 +8,12 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
-import './PopularInstructor.css'
+import "./PopularInstructor.css";
 // import required modules
 import { FreeMode, Pagination } from "swiper";
 const PopularInstructors = () => {
-  
   const [classes, setClasses] = useState([]);
-  
+
   useEffect(() => {
     fetch("classes.json")
       .then((res) => res.json())
@@ -28,12 +27,12 @@ const PopularInstructors = () => {
   console.log(classes);
   return (
     <div>
-              <h1 className="uppercase text-3xl text-center mb-12 font-bold">
-        Achieve your goals with our Popular Classes
+      <h1 className="uppercase text-3xl text-center mb-12 font-bold">
+        Popular Instructors{" "}
       </h1>
-        <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={2}
         freeMode={true}
         pagination={{
           clickable: true,
@@ -41,28 +40,23 @@ const PopularInstructors = () => {
         modules={[FreeMode, Pagination]}
         className="mySwiper"
       >
-
-
-{classes.map((singleClass) => (
+        {classes.map((singleClass) => (
           <SwiperSlide
             key={singleClass.id}
             singleClass={singleClass}
             className="custom "
           >
             <img
-              className=" ms-20 h-[300px] w-[350px]"
-              src={singleClass.classPicture}
+              className="rounded-xl ms-24 h-[200px] w-[200px]"
+              src={singleClass.instructorImage}
               alt=""
             />
             <h3 className=" uppercase text-center text-black-400 mb-16  font-bold ">
-              {singleClass.className}
+              {singleClass.instructorName}
             </h3>
           </SwiperSlide>
         ))}
-
-
       </Swiper>
-      
     </div>
   );
 };
