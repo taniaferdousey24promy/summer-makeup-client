6532319cc4153e87cd1b80cc0634407291efad50
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-06-10 18:14:59 +0600

    Commiting the banner

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -124,16 +124,28 @@ const Banner = () => {
               </h3>
             </div>
 
-            <h3 className=" font text-2xl text-pink-800 mt-4 mb-20 mt-16">
+            <h3 className=" font text-2xl text-black mt-4  mt-16">
               signup by 6-28, 2023
             </h3>
+            <h3 className=" text-2xl font text-pink-800 mt-12 ">
+            Hands-on & Live Demo Group Class
+            </h3>
+      
+            <h3 className="  font  mt-5 mb-4">
+            1-On-1 Session
+
+            </h3>
+         
+
+            
+            
           </div>
         </SwiperSlide>
 
         <SwiperSlide className=" brightness-75">
           <img
             className=" h-[700px] w-[2000px] me-40"
-            src="https://i.postimg.cc/xCGYN5kp/28448742259-bc865057b1-b.jpg"
+            src="https://i.postimg.cc/NFtrX9S4/318045-1600x1067-applying-lip-gloss.jpg"
             alt=""
           />
 
@@ -150,7 +162,7 @@ const Banner = () => {
               <h1 className="mt-12">Content</h1>
 
               <ul className=" lowercase list-disc pl-6">
-                <li >Skin Preparation</li>
+                <li>Skin Preparation</li>
                 <li>Application Strategies</li>
                 <li>Bases</li>
                 <li>Illumination</li>
@@ -159,70 +171,46 @@ const Banner = () => {
                 <li>Eye Makeup</li>
               </ul>
 
-              <p className=" mt-12 lowercase">Makeup-classes | www.youwhere.com</p>
-              <p className="lowercase">Free makeup classes flyer template from EDIT.org</p>
+              <p className=" mt-12 lowercase">
+                Makeup-classes | www.youwhere.com
+              </p>
+              <p className="lowercase">
+                Free makeup classes flyer template from EDIT.org
+              </p>
             </div>
-            {/* 
-            <h1 className="mt-12">Content</h1>
-            <ol type="1">
-              <li>Skin Preparation</li>
-            </ol> */}
-
-            
-            {/* <h3 className=" font uppercase text-5xl text-white font-bold mb-8 ">
-              Summer Camp
-            </h3>
-
-            <h3 className=" text-2xl font text-pink-800 mt-5 mb-12">
-              Creativity and professional
-            </h3>
-
-            <div className="w-[500px]">
-              <h3 className="mt-4  w-50">
-                Welcome to our Summer Camp Makeup Classes! Get ready to enhance
-                your skills, explore new techniques, and unleash your
-                creativity. Join us for an unforgettable summer filled with
-                makeup magic!
-              </h3>
-            </div>
-
-            <h3 className=" font text-2xl text-pink-800 mt-4 mb-20 mt-16">
-              signup by 6-28, 2023
-            </h3> */}
           </div>
         </SwiperSlide>
-        <SwiperSlide className="brightness-50">
+        <SwiperSlide className="brightness-75c">
           <img
             className=" h-[700px] w-[2000px] me-40"
             src="https://i.ibb.co/6P1jwMf/friends-getting-ready-drag-show-23-2149490841.jpg"
             alt=""
           />
 
-          {/* <div className="   -mt-[600px] text-left   ml-40">
-            <h3 className=" font uppercase text-5xl text-black-400 mb-8 font-bold ">
-              2023
-            </h3>
-            <h3 className=" font uppercase text-5xl text-black-400 font-bold mb-8 ">
-              Summer Camp
-            </h3>
+          <div className="m-4 absolute bottom-10 left-30 right-20 top-10 px-4 py-2 bg-gray-800 opacity-70">
+            <div className=" mt-8 mx-8 font uppercase  text-white mb-8 font-bold">
+              <h1 className="text-3xl">Beginners <br /> <h1 className="mt-8">1.0</h1> </h1>
 
-            <h3 className=" text-2xl font text-pink-800 mt-5 mb-12">
-              Creativity and professional
-            </h3>
+              <br />
+              <h1 className="text-2xl">the 20-minute face</h1>
+              <br />
 
-            <div className="w-[500px]">
-              <h3 className="mt-4  w-50">
-                Welcome to our Summer Camp Makeup Classes! Get ready to enhance
-                your skills, explore new techniques, and unleash your
-                creativity. Join us for an unforgettable summer filled with
-                makeup magic!
-              </h3>
+              {/* <h1 className="lowercase">by write your name here</h1> */}
+              <h1 className="mt-12 text-cyan-400">Skin prep</h1>
+              <h1 className="lowercase">Lorem ipsum dolor sit amet.</h1>
+
+              <h1 className="mt-12 text-cyan-400">Foundation</h1>
+              <h1 className="lowercase">Lorem ipsum dolor sit amet.</h1>
+
+              <h1 className="mt-12 text-cyan-400" >Highlight</h1>
+              <h1 className="lowercase">Lorem ipsum dolor sit amet.</h1>
+
+
+
+              
+
             </div>
-
-            <h3 className=" font text-2xl text-pink-800 mt-4 mb-20 mt-16">
-              signup by 6-28, 2023
-            </h3>
-          </div> */}
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
