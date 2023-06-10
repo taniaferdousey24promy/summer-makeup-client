773462e3a7374e87aca93cedb87509c2aacbe1be
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-06-11 00:09:50 +0600

    Commiting extra section in home page

--- src/Pages/Home/ExtraSection/ExtraSection.jsx
+++ src/Pages/Home/ExtraSection/ExtraSection.jsx
@@ -1,107 +1,140 @@
-import * as React from "react";
-import {
-  useTrail,
-  useChain,
-  useSprings,
-  animated,
-  useSpringRef,
-} from "@react-spring/web";
+import { useRef } from "react";
+import { styled } from "@stitches/react";
+import { useTrail, animated } from "@react-spring/web";
+import { HiSparkles } from "react-icons/hi";
 
-import styles from './styles.module.css'
+const AppContainer = styled("div", {
+  width: "25vw",
+  height: "25vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  marginLeft: 500,
+});
 
-const COORDS = [
-  [50, 30],
-  [90, 30],
-  [50, 50],
-  [60, 60],
-  [70, 60],
-  [80, 60],
-  [90, 50],
-];
+const Container = styled("div", {
+  display: "flex",
+  gap: 10,
+  // marginBottom: 80,
+});
 
-const STROKE_WIDTH = 0.5;
+const Box = styled("div", {
+  position: "relative",
+  height: 50,
+  width: 50,
+});
 
-const OFFSET = STROKE_WIDTH / 2;
+const SharedStyles = {
+  width: "100%",
+  height: "100%",
+  position: "absolute",
+  inset: 0,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontFamily: "Helvetica",
+  fontWeight: 800,
+  backfaceVisibility: "hidden",
+};
 
-const MAX_WIDTH = 150 + OFFSET * 2;
-const MAX_HEIGHT = 100 + OFFSET * 2;
+const FrontBox = styled(animated.div, {
+  ...SharedStyles,
+  backgroundColor: "#fafafa",
+  border: "solid 2px #1a1a1a",
+});
+
+const BackBox = styled(animated.div, {
+  ...SharedStyles,
+  backgroundColor: "#6cab64",
+  border: "solid 2px #6cab64",
+  color: "#fafafa",
+});
+
+const items = ["D", "I", "S", "C", "O", "U", "N", "T"];
 const ExtraSection = () => {
-  const gridApi = useSpringRef();
-
-  const gridSprings = useTrail(16, {
-    ref: gridApi,
-    from: {
-      x2: 0,
-      y2: 0,
-    },
-    to: {
-      x2: MAX_WIDTH,
-      y2: MAX_HEIGHT,
-    },
-  });
-
-  const boxApi = useSpringRef();
-
-  const [boxSprings] = useSprings(7, (i) => ({
-    ref: boxApi,
-    from: {
-      scale: 0,
-    },
-    to: {
-      scale: 1,
-    },
-    delay: i * 200,
-    config: {
-      mass: 2,
-      tension: 220,
-    },
+  const [trail, api] = useTrail(items.length, () => ({
+    rotateX: 0,
   }));
 
-  useChain([gridApi, boxApi], [0, 1], 1500);
+  const isFlipped = useRef(false);
+
+  const handleClick = () => {
+    if (isFlipped.current) {
+      api.start({
+        rotateX: 0,
+      });
+      isFlipped.current = false;
+    } else {
+      api.start({
+        rotateX: 180,
+      });
+      isFlipped.current = true;
+    }
+  };
   return (
-    <div className={styles["background-container"]}>
-      <div className={styles.container}>
-        <svg viewBox={`0 0 ${MAX_WIDTH} ${MAX_HEIGHT}`}>
-          <g>
-            {gridSprings.map(({ x2 }, index) => (
-              <animated.line
-                x1={0}
-                y1={index * 10 + OFFSET}
-                x2={x2}
-                y2={index * 10 + OFFSET}
-                key={index}
-                strokeWidth={STROKE_WIDTH}
-                stroke="currentColor"
-              />
-            ))}
-            {gridSprings.map(({ y2 }, index) => (
-              <animated.line
-                x1={index * 10 + OFFSET}
-                y1={0}
-                x2={index * 10 + OFFSET}
-                y2={y2}
-                key={index}
-                strokeWidth={STROKE_WIDTH}
-                stroke="currentColor"
-              />
-            ))}
-          </g>
-          {boxSprings.map(({ scale }, index) => (
-            <animated.rect
-              key={index}
-              width={10}
-              height={10}
-              fill="currentColor"
-              style={{
-                transformOrigin: `${5 + OFFSET * 2}px ${5 + OFFSET * 2}px`,
-                transform: `translate(${COORDS[index][0] + OFFSET}px, ${
-                  COORDS[index][1] + OFFSET
-                }px)`,
-                scale,
-              }}
-            />
+    <div>
+      <AppContainer>
+        <Container onClick={handleClick}>
+          {trail.map(({ rotateX }, i) => (
+            <Box key={i}>
+              <FrontBox
+                key={items[i]}
+                style={{
+                  transform: rotateX.to(
+                    (val) => `perspective(600px) rotateX(${val}deg)`
+                  ),
+                  transformStyle: "preserve-3d",
+                }}
+              >
+                {"?"}
+              </FrontBox>
+              <BackBox
+                style={{
+                  transform: rotateX.to(
+                    (val) => `perspective(600px) rotateX(${180 - val}deg)`
+                  ),
+                  transformStyle: "preserve-3d",
+                }}
+              >
+                {items[i]}
+              </BackBox>
+            </Box>
           ))}
-        </svg>
+        </Container>
+      </AppContainer>
+
+      <div className="hero  bg-base-100 mb-20 rounded-xl">
+        <div className="hero-content flex-col lg:flex-row gap-0">
+          <img
+            src="https://i.postimg.cc/Gp8LYtqV/The-Anti-Aging-Contour-Trick-Every-Woman-Over-40-Should-Try.jpg"
+            className="w-[500px] h-[400px]  shadow-2xl"
+          />
+          <div className=" w-[1000px] bg-[#FBCEB1] h-[400px] p-12">
+            <h1 className="text-4xl font-bold">
+              SURPRISE on Smokey Eye Makeup Course!
+            </h1>
+            
+            <div className="flex py-6 gap-2 ">
+              <HiSparkles className="mt-2  "></HiSparkles>
+              <p>
+                {" "}
+                <span className="font-bold"> Customized guidance: :</span> Learn
+                the techniques, tips, and tricks to create sultry and
+                mesmerizing smokey eye looks.
+              </p>
+            </div>
+            <div className="flex py-6 gap-2 ">
+              <HiSparkles className="mt-2  "></HiSparkles>
+              <p>
+                {" "}
+                <span className="font-bold"> Exclusive bonuses: :</span> Gain
+                confidence in your makeup skills and unlock your creativity with
+                personalized guidance and exclusive bonuses.
+              </p>
+            </div>
+            <button className="m-4 btn btn-primary">Buy Now</button>
+          </div>
+        </div>
       </div>
     </div>
   );
