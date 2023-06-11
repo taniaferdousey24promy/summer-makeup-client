82abb91645291306604a861a0a97feb24ee0d913
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-06-11 18:12:22 +0600

    Commiting all classes updated

--- src/Pages/AllClasses/AllClasses.jsx
+++ src/Pages/AllClasses/AllClasses.jsx
@@ -8,7 +8,7 @@ const AllClasses = () => {
   );
   return (
     <div className=" mb-20">
-      <p className="mb-12 mt-20 text-left text-4xl">Makeup Classes</p>
+      <p className="mb-12 mt-20 text-center text-4xl">Available Makeup Courses</p>
 
       <div className=" grid  grid-cols-3 gap-20  ">
         {approved.map((singleClasses) => (
@@ -27,9 +27,13 @@ const AllClasses = () => {
                   <div className="badge badge-secondary">APPROVED</div>
                 </h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, facere.?</p>
+                <p className="card-title">Price : <span>Only {singleClasses.price}</span></p>
+
                 <div className="card-actions justify-end">
-                  <div className="badge badge-outline">Weekly</div>
+                  {/* <div className="badge badge-outline">Weekly</div> */}
+                  <div className="badge badge-outline">Availbale seatc {singleClasses.availableSeats}</div>
                   <div className="badge badge-outline"> by {singleClasses.instructorName}</div>
+                  {/* <div className="badge badge-outline"> Price: {singleClasses.price}</div> */}
                 </div>
               </div>
             </div>
